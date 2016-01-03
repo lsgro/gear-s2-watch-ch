a810a0f1f34425095f921c7bc44f39f9590791ab
lsgro <luigi.sgro@gmail.com>
2016-01-03 21:08:51 +0100

    Added code

---
+++ inc/watchapplication.h
@@ -0,0 +1,18 @@
+#ifndef __anotherwatch_H__
+#define __anotherwatch_H__
+
+#include <watch_app.h>
+#include <watch_app_efl.h>
+#include <Elementary.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "anotherwatch"
+
+#if !defined(PACKAGE)
+#define PACKAGE "com.luigisgro.anotherwatch"
+#endif
+
+#endif /* __anotherwatch_H__ */
